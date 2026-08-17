35c68cb7f4434b9cf821524206f95d4180a4cede
prateekcholkar80 <prateek.cholkar@gmail.com>
2026-08-17 22:53:56 +0530

    Add historical candle retrieval using Angel One API

--- app/services/market_data.py
+++ app/services/market_data.py
@@ -25,3 +25,28 @@ class MarketDataService:
         )
 
         return response
+
+    def get_historical_candles(
+        self,
+        exchange,
+        symbol_token,
+        interval,
+        from_date,
+        to_date
+    ):
+
+        if self.client is None:
+            raise Exception(
+                "Client not initialized. Call initialize() first."
+            )
+
+        params = {
+            "exchange": exchange,
+            "symboltoken": symbol_token,
+            "interval": interval,
+            "fromdate": from_date,
+            "todate": to_date
+        }
+
+        response = self.client.getCandleData(params)
+        return response
